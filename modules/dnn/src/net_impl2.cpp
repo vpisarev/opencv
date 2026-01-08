014275e53c475896febd047232813ef0dc7971dd
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2026-01-08 22:17:00 +0300

    trying to make all dnn tests pass

--- modules/dnn/src/net_impl2.cpp
+++ modules/dnn/src/net_impl2.cpp
@@ -291,8 +291,8 @@ void Net::Impl::prepareForInference()
     if (!prepared) {
         constFold();
         constArgs();        
-        useBlockLayout();
         fuseBasic();
+        useBlockLayout();        
         assignBuffers();
         totalLayers = updateGraphOfs(mainGraph, 0, true);
         prepared = true;
